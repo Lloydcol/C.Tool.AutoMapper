cbc1d52f546258afdad3f5fc0d7d869faef04a65
Colart Lloyd <55253106+Lloydcol@users.noreply.github.com>
2021-06-18 16:17:28 +0200

    Init

---
+++ MapperExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ToolBox.AutoMapper
+{
+    public static class MapperExtensions
+    {
+        public static T MapTo<T>(this object from)
+           where T: new()
+        {
+            Type fromType = from.GetType();
+            T result = new T();
+            var toProps = typeof(T).GetProperties();
+            foreach(var prop in toProps)
+            {
+                var fromProp = fromType.GetProperty(prop.Name);
+                if (fromProp != null)
+                {
+                    object value = fromProp.GetValue(from);
+                    try
+                    {
+                        prop.SetValue(result, value);
+                    } catch(Exception ) { }
+                }
+            }
+            return result;
+        }
+    }
+}
